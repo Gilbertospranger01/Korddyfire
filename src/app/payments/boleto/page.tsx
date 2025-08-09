5b43e99541081674770da53fee5799b2d57319eb
Gilberto Spranger <malvespranger2004@gmail.com>
2025-08-09 04:06:55 +0100

    Atualizar o page.tsx

--- src/app/payments/boleto/page.tsx
+++ src/app/payments/boleto/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Loadingpage from "../../../loadingpages/loadingpage";
+import Loadingpage from "@/loadingpages/loadingpage";
 import { useState } from "react";
 
 export default function BoletoPaymentPage() {
