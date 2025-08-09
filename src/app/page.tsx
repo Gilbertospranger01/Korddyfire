97e13c69d31a2c5af28ca1ed0bc3e627b39840f8
Gilberto Spranger <malvespranger2004@gmail.com>
2025-08-09 04:16:40 +0100

    Atualizar o page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { useAuth } from "../hooks/useAuth";
+import { useAuth } from "@/hooks/useAuth";
 import { usePathname, useRouter } from "next/navigation";
 import api from "@/utils/api";
 import Loadingconnection from "@/loadingpages/loadingconnection";
