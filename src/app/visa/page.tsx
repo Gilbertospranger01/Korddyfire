fc7d3002078d08774a029777f53141d2235486c9
Gilberto Spranger <mauvespranger01@gmail.com>
2025-09-04 00:52:40 +0100

    Update page.tsx

--- src/app/visa/page.tsx
+++ src/app/visa/page.tsx
@@ -1,13 +1,7 @@
-import Loadingpage from "@/loadingpages/loadingpage";
 import React from "react";
 
 function Visa() {
 
-  if (!session) {
-    return (
-      <Loadingpage />
-    );
-  }
   return (
     <div>
       <h1>Visa</h1>
