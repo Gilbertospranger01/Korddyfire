a1d0649cd41dad87475910d7c36162e8a7e371bc
Gilberto Spranger <mauvespranger01@gmail.com>
2025-09-03 21:14:15 +0100

    Update page.tsx

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,13 +1,7 @@
-import Loadingpage from "@/loadingpages/loadingpage";
 import React from "react";
 
 function Settings() {
 
-  if (!session) {
-    return (
-      <Loadingpage />
-    );
-  }
   return (
     <div>
       <h1>Settings</h1>
