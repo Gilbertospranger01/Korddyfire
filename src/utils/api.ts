4b147c063fef06fcfe0e26dc6c07510e86d704db
Gilberto Spranger <mauvespranger01@gmail.com>
2025-09-03 13:43:38 +0100

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,27 +1,36 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+
+export type ApiErrorResponse = {
+  error: string;
+};
 
 const api = axios.create({
   baseURL: "http://localhost:4000/api/",
-  withCredentials: true, 
+  withCredentials: true,
   headers: {
-    "Content-Type": "application/json", 
-    Accept: "*/*", 
+    "Content-Type": "application/json",
+    Accept: "*/*",
   },
 });
 
 api.interceptors.request.use((config) => {
   const isUpload = config.headers["Content-Type"] === "multipart/form-data";
   if (isUpload) {
-    delete config.headers["Content-Type"]; 
+    delete config.headers["Content-Type"];
   }
   return config;
 });
 
-api.interceptors.response.use((response) => {
-  if (response.request.responseType === "blob") {
+api.interceptors.response.use(
+  (response) => {
+    if (response.request.responseType === "blob") {
+      return response;
+    }
     return response;
+  },
+  (error: AxiosError<ApiErrorResponse>) => {
+    return Promise.reject(error);
   }
-  return response;
-});
+);
 
 export default api;
