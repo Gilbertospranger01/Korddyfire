87dfd3e5d04e48110058d594178c37a838663376
Gilberto Spranger <mauvespranger01@gmail.com>
2025-09-05 00:37:30 +0100

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,7 +12,7 @@ const handler = NextAuth({
       id: "imlinkedy",
       name: "Imlinkedy",
       type: "oauth",
-      version: "2.0",
+      version: "1.0",
       clientId: process.env.IMLINKEDY_CLIENT_ID!,
       clientSecret: process.env.IMLINKEDY_CLIENT_SECRET!,
       authorization: {
