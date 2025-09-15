9d91207b765baba0e7057036a4c6ef72db6bd9e8
Gilberto Spranger <malvespranger2004@gmail.com>
2025-09-15 19:12:49 +0100

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -41,7 +41,9 @@ const authOptions: AuthOptions = {
   providers: [
     GoogleProvider({ clientId: process.env.GOOGLE_CLIENT_ID!, clientSecret: process.env.GOOGLE_CLIENT_SECRET! }),
     FacebookProvider({ clientId: process.env.FACEBOOK_CLIENT_ID!, clientSecret: process.env.FACEBOOK_CLIENT_SECRET! }),
-    GitHubProvider({ clientId: process.env.GITHUB_CLIENT_ID!, clientSecret: process.env.GITHUB_CLIENT_SECRET! }),
+ 
+    GithubProvider({ clientId: process.env.GITHUB_ID,
+clientSecret: process.env.GITHUB_SECRET,}),
     CredentialsProvider({
       id: "imlinkedy",
       name: "Imlinkedy",
