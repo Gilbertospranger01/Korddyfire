15862e83d0b133928f54bd14267580acb82ad4b5
Gilberto Spranger <malvespranger2004@gmail.com>
2025-09-16 03:56:46 +0100

    Update page.tsx

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -76,7 +76,7 @@ export default function Signin() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: provider as "google" | "facebook" | "github",
         options: {
-          redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/callback`,
+          redirectTo: "https://korddyfire.vercel.app/api/auth/callback",
         },
       });
 
