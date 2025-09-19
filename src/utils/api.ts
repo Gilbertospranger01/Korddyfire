ae37f3fd609d21b3b17edf009ae1559c239aee5b
Gilberto Spranger <malvespranger2004@gmail.com>
2025-09-19 11:59:17 +0100

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,7 +5,7 @@ export type ApiErrorResponse = {
 };
 
 const api = axios.create({
-  baseURL: "https://korddyfirebases.onrender.com/api/v1",
+  baseURL: "https://korddyfirebases.onrender.com/api/v1/",
   withCredentials: true, // cookies cross-domain
   headers: {
     Accept: "application/json", // mantém só Accept
