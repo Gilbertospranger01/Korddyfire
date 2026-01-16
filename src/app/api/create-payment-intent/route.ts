5fa7ea29235e0c95f15071afbce6f879d6d8d9f7
Gilberto Spranger <malvespranger2004@gmail.com>
2026-01-17 00:04:34 +0100

    Update route.ts

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 import api from '@/utils/api';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: '2025-09-30.clover',
+  apiVersion: '2025-12-15.clover',
 });
 
 interface RequestBody {
